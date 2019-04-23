be9cda018b2d014ee0e87af8b9f87b10801ff3eb
Muzhen Li <muzhenli@Muzhens-MacBook-Air.local>
2019-04-23 16:16:10 -0400

    text pg1

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetDayViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetDayViewController.swift
@@ -11,13 +11,22 @@ import UIKit
 class StreetDayViewController: UIViewController, UITabBarControllerDelegate {
     
     var stage = 0 // to control stage
+    var count = 0
+    let text = ["A dead leaf fell at your feet.",
+                "This is a sign that winter is coming",
+                "You used to live on the bench at Madison square, but now, you need another place to stay.",
+                "3 month in the prison on the island could be a good choice.",
+                "This means 3 month of food every day and bed every night.",
+                "You come to the street.",
+                "There are many ways of doing this so that cop arrrest you to the jail"]
+    lazy var size = text.count
     @IBOutlet weak var choice1: UIButton!
     @IBOutlet weak var choice2: UIButton!
     @IBOutlet weak var tf: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Set gesture to menu
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
         let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
@@ -37,34 +46,38 @@ class StreetDayViewController: UIViewController, UITabBarControllerDelegate {
             self.choice1.isHidden = true
             self.choice2.isHidden = true
             print("stage is 0" )
-            tf.text = "It's winter and it's cold."
+            tf.text = text[count]
         case 1:
             // show choices
             self.choice1.isHidden = false
             choice1.setTitle("try something", for: .normal)
             //self.choice2.isHidden = false
             print("stage is 1")
-            tf.text = "I want to go to jail"
+            tf.text = text[count]
         default:
             // do nothing
             break
         }
     }
     
-    // function used for swipe back to menu 
+    // function used for swipe back to menu
     @objc func handleSwipe(sender:UISwipeGestureRecognizer){
         if (sender.state == .ended){
             performSegue(withIdentifier: "toMenu", sender: self)
         }
     }
-
+    
     @IBAction func tapText(_ sender: UITapGestureRecognizer) {
         print("text tapped")
-        if (stage<3){
-            stage = stage+1
-            gotoStage()
+        if (count<size-1){
+            count = count+1
         }
+        else{
+            stage = 1
+        }
+        gotoStage()
     }
+    
     
     
     /*
