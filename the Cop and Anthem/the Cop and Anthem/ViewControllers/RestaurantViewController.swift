1e0b77546ed1f8806bfdf42c02ccd60fd2a7aa87
April <owo@outlook.com>
2019-04-23 16:02:47 -0400

    restaurant logic completed

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/RestaurantViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/RestaurantViewController.swift
@@ -15,7 +15,7 @@ class RestaurantViewController: UIViewController {
     @IBOutlet weak var tryElseBtn: UIButton!
     @IBOutlet weak var tf: UITextView!
     
-    var stage = 1
+    var stage = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,6 +48,20 @@ class RestaurantViewController: UIViewController {
             choice1.setTitle("say #1", for: .normal)
             choice2.setTitle("say #2", for: .normal)
             tf.text = "Waiter: You can't afford here. Get out. "
+        case 2:
+            //choice 1
+            tf.text = "bad choice 1"
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = false
+            self.stepInsideBtn.isHidden = true
+        case 3:
+            // choice 2
+            tf.text = "bad choice 2"
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = false
+            self.stepInsideBtn.isHidden = true
         default:
             // do nothing
             break
@@ -62,7 +76,23 @@ class RestaurantViewController: UIViewController {
     }
     
     @IBAction func stepInsidePressed(_ sender: UIButton) {
+        stage = 1
+        gotoStage()
     }
+    
+    // what happen when choice1 pressed
+    @IBAction func choice1Pressed(_ sender: UIButton) {
+        stage = 2
+        gotoStage()
+    }
+    
+    // what happen when choice2 pressed
+    @IBAction func choice2Pressed(_ sender: UIButton) {
+        stage = 3
+        gotoStage()
+    }
+    
+    
     
     /*
     // MARK: - Navigation
