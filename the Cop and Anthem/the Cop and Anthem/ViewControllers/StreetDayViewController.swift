0941c2dd658e22a59f619f9d5f5480c743c7407f
April <owo@outlook.com>
2019-04-23 13:22:57 -0400

    street day interaction
    logic only, need to fill text

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetDayViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetDayViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class StreetDayViewController: UIViewController, UITabBarControllerDelegate {
     
+    var stage = 0 // to control stage
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var tf: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +24,48 @@ class StreetDayViewController: UIViewController, UITabBarControllerDelegate {
         upSwipe.direction = .up
         view.addGestureRecognizer(rightSwipe)
         view.addGestureRecognizer(upSwipe)
-
+        
+        // go to stage 0
+        gotoStage()
+    }
+    
+    // do stuff accoring int stage
+    func gotoStage(){
+        switch stage{
+        case 0:
+            // hide choices
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            print("stage is 0" )
+            tf.text = "It's winter and it's cold."
+        case 1:
+            // show choices
+            self.choice1.isHidden = false
+            choice1.setTitle("try something", for: .normal)
+            //self.choice2.isHidden = false
+            print("stage is 1")
+            tf.text = "I want to go to jail"
+        default:
+            // do nothing
+            break
+        }
     }
     
     // function used for swipe back to menu 
     @objc func handleSwipe(sender:UISwipeGestureRecognizer){
         if (sender.state == .ended){
-            performSegue(withIdentifier: "toTab", sender: self)
+            performSegue(withIdentifier: "toMenu", sender: self)
+        }
+    }
+
+    @IBAction func tapText(_ sender: UITapGestureRecognizer) {
+        print("text tapped")
+        if (stage<3){
+            stage = stage+1
+            gotoStage()
         }
     }
     
-    
-    // if text is touched
-    @IBAction func touchText(_ sender: UITapGestureRecognizer) {
-        // next text
-    }
     
     /*
     // MARK: - Navigation
