5271e8d832af07c7bf3ff2b90939e62beeb66b9c
April <owo@outlook.com>
2019-04-23 15:56:13 -0400

    Restaurant basic logic
    4 buttons

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/RestaurantViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/RestaurantViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class RestaurantViewController: UIViewController {
-
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var stepInsideBtn: UIButton!
+    @IBOutlet weak var tryElseBtn: UIButton!
+    @IBOutlet weak var tf: UITextView!
+    
+    var stage = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +26,32 @@ class RestaurantViewController: UIViewController {
         view.addGestureRecognizer(rightSwipe)
         view.addGestureRecognizer(upSwipe)
         
+        gotoStage()
+    }
+    
+    // do stuff accoring int stage
+    func gotoStage(){
+        switch stage{
+        case 0:
+            // bg story
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = true
+            self.stepInsideBtn.isHidden = false
+            tf.text = "Let's do it"
+        case 1:
+            // waiter talk
+            self.choice1.isHidden = false
+            self.choice2.isHidden = false
+            self.tryElseBtn.isHidden = true
+            self.stepInsideBtn.isHidden = true
+            choice1.setTitle("say #1", for: .normal)
+            choice2.setTitle("say #2", for: .normal)
+            tf.text = "Waiter: You can't afford here. Get out. "
+        default:
+            // do nothing
+            break
+        }
     }
     
     // function used for swipe back to menu
@@ -29,7 +61,9 @@ class RestaurantViewController: UIViewController {
         }
     }
     
-
+    @IBAction func stepInsidePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
