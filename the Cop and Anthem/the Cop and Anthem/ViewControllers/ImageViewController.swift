8a41b213f24fc6b34a302f8a95a2c12709cc4808
Muzhen Li <muzhenli@Muzhens-MacBook-Air.local>
2019-04-24 00:08:13 -0400

    archive

---
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  the Cop and Anthem
+//
+//  Created by Muzhen Li on 4/24/19.
+//  Copyright © 2019 April Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
