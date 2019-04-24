ef4e02a15a34894c04abba262a11b1e82d36406f
April <owo@outlook.com>
2019-04-24 19:33:06 -0400

    auto layout for game scene

--- the Cop and Anthem/the Cop and Anthem/ViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewController.swift
@@ -19,19 +19,6 @@ class ViewController: UIViewController {
         var img_bools = defaults.object(forKey: "stored_bool_list") as? [Bool] ?? [false,true,true,true,true,true,true,true,true]
         defaults.set(img_bools, forKey: "stored_bool_list")
     }
-
-    @IBAction func btnPressed(_ sender: UIButton) {
-        //create variable
-        let defaults = UserDefaults.standard
-        // retrieve the stored list from the phone
-        var img_bools = defaults.object(forKey: "stored_bool_list") as? [Bool] ?? [false,true,true,true,true,true,true,true,false]
-        
-        // store the generated game code on the user's phone
-        defaults.set(img_bools, forKey: "stored_bool_list")
-        
-        print("btn Pressed")
-        print(img_bools)
-    }
     
 }
 
