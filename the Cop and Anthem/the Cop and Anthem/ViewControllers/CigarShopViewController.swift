1387cabac81c44a257c5e12b46ac54897af82bb0
April <owo@outlook.com>
2019-04-29 18:53:47 -0400

    street night logic

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/CigarShopViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/CigarShopViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class CigarShopViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetNightViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetNightViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class StreetNightViewController: UIViewController {
-
+    var stage = 0
+    var count = 0
+    @IBOutlet weak var tf: UITextView!
+    @IBOutlet weak var choice1: UIButton!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var tryElseBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +26,36 @@ class StreetNightViewController: UIViewController {
         view.addGestureRecognizer(rightSwipe)
         view.addGestureRecognizer(upSwipe)
         
+        gotoStage()
+    }
+    
+    // do stuff accoring int stage
+    func gotoStage(){
+        switch stage{
+        case 0:
+            // bg story
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = true
+        case 1:
+            // wait to choose
+            self.choice1.isHidden = false
+            self.choice2.isHidden = false
+            self.tryElseBtn.isHidden = true
+        case 2:
+            //choice 1
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = false
+        case 3:
+            // choice 2
+            self.choice1.isHidden = true
+            self.choice2.isHidden = true
+            self.tryElseBtn.isHidden = true
+        default:
+            // do nothing
+            break
+        }
     }
     
     // function used for swipe back to menu
