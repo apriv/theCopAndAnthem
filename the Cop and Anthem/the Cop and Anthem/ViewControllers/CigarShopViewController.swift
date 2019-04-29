9024144981bc22ae709cb1bb320f49095af6d823
April <owo@outlook.com>
2019-04-29 19:13:42 -0400

    umbrella logic

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/CigarShopViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/CigarShopViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 
 class CigarShopViewController: UIViewController {
+    var stage = 0
+    var count = 0
     
-
+    @IBOutlet weak var tf: UITextView!
+    @IBOutlet weak var findBtn: UIButton!
+    @IBOutlet weak var tryBtn: UIButton!
+    @IBOutlet weak var umbrellaBox: UIButton!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +28,64 @@ class CigarShopViewController: UIViewController {
         upSwipe.direction = .up
         view.addGestureRecognizer(rightSwipe)
         view.addGestureRecognizer(upSwipe)
-        
+
+        gotoStage()
+    }
+    
+    
+    // do stuff accoring int stage
+    func gotoStage(){
+        switch stage{
+        case 0:
+            // hide choices, bg story
+            tf.text = "you find a cigar shop."
+            tf.isHidden = false
+            findBtn.isHidden = true
+            tryBtn.isHidden = true
+            umbrellaBox.isHidden = true
+            bgImg.image = UIImage(named:"restaurant_umbrella")
+        case 1:
+            // find sth btn
+            tf.text = "find something to rob"
+            tf.isHidden = false
+            findBtn.isHidden = false
+            tryBtn.isHidden = true
+            umbrellaBox.isHidden = true
+        case 2:
+            // to find
+            tf.isHidden = true
+            findBtn.isHidden = true
+            tryBtn.isHidden = true
+            umbrellaBox.isHidden = false
+        case 3:
+            // found and end lines
+            tf.text = "found"
+            bgImg.image = UIImage(named:"restaurant_dark")
+            tf.isHidden = false
+            findBtn.isHidden = true
+            tryBtn.isHidden = false
+            umbrellaBox.isHidden = true
+        default:
+            // do nothing
+            break
+        }
+    }
+    
+    @IBAction func findPressed(_ sender: UIButton) {
+        stage = 2
+        gotoStage()
+    }
+    
+    @IBAction func umbFound(_ sender: UIButton) {
+        stage = 3
+        gotoStage()
+    }
+    
+    @IBAction func tfPressed(_ sender: UITapGestureRecognizer) {
+        if(stage==0){
+            stage = 1
+            gotoStage()
+        }
     }
     
     // function used for swipe back to menu
