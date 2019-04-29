8cb60d07db9e6ac5206526457ce49d6fc6e98b66
Muzhen Li <muzhenli@Muzhens-MacBook-Air.local>
2019-04-29 19:45:22 -0400

    street text

--- the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetNightViewController.swift
+++ the Cop and Anthem/the Cop and Anthem/ViewControllers/StreetNightViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class StreetNightViewController: UIViewController {
     var stage = 0
     var count = 0
+    let text = ["The cop turned his back, said to another man.",
+                "Cop: It 's just one of those college boys.",
+                "Cop: He won't hurt anything.",
+                "Cop: We had orders to let them shout."]
+    lazy var size = text.count
     @IBOutlet weak var tf: UITextView!
     @IBOutlet weak var choice1: UIButton!
     @IBOutlet weak var choice2: UIButton!
@@ -34,6 +39,7 @@ class StreetNightViewController: UIViewController {
         switch stage{
         case 0:
             // bg story
+            tf.text = "You began to shout as if you have drank too much."
             self.choice1.isHidden = true
             self.choice2.isHidden = true
             self.tryElseBtn.isHidden = true
@@ -42,16 +48,30 @@ class StreetNightViewController: UIViewController {
             self.choice1.isHidden = false
             self.choice2.isHidden = false
             self.tryElseBtn.isHidden = true
+            choice1.setTitle("\"Stupid Cop!\"", for: .normal)
+            choice2.setTitle("\"How is your night!\"", for: .normal)
         case 2:
             //choice 1
+            tf.text = text[count]
             self.choice1.isHidden = true
             self.choice2.isHidden = true
-            self.tryElseBtn.isHidden = false
+            if(count == size-1){
+                self.tryElseBtn.isHidden = false
+            }
+            else{
+                self.tryElseBtn.isHidden = true
+            }
         case 3:
             // choice 2
+            tf.text = text[count]
             self.choice1.isHidden = true
             self.choice2.isHidden = true
-            self.tryElseBtn.isHidden = true
+            if(count == size-1){
+                self.tryElseBtn.isHidden = false
+            }
+            else{
+                self.tryElseBtn.isHidden = true
+            }
         default:
             // do nothing
             break
@@ -64,7 +84,33 @@ class StreetNightViewController: UIViewController {
             performSegue(withIdentifier: "toMenu", sender: self)
         }
     }
-
+    
+    
+    @IBAction func Taptext(_ sender: Any) {
+        if(stage == 2 || stage == 3){
+            if (count<size-1){
+                count = count+1
+            }
+        }
+        if(stage == 0){
+            stage = 1
+        }
+        gotoStage()
+    }
+    
+    @IBAction func choice1Pressed(_ sender: UIButton) {
+        stage = 2
+        gotoStage()
+    }
+    
+    // what happen when choice2 pressed
+    @IBAction func choice2Pressed(_ sender: UIButton) {
+        stage = 3
+        gotoStage()
+    }
+    
+    
+    
     
 
     /*
